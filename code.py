f7dc895e1a078358179f302e7249d3f63cad6105
mirikgitakk@gmmail.com <mirikgitakk@gmail.com>
2024-03-03 18:42:08 +0300

    first Ban Filter

---
+++ code.py
@@ -0,0 +1,26 @@
+# import requests
+# import pprint
+# def giveAnimation(answer: str) -> str:
+#     endPoint = f'https://yesno.wtf/api?force={answer}'
+#     res = requests.get(endPoint).json()
+#     return res['image']
+#
+#
+# import time
+# offset = -2
+# while True:
+#     #получить информацию по всем событиям (апдейтам)
+#     endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+#     param = {'offset': offset + 1}
+#     response = requests.get(endPoint, params=param).json()
+#     if response['result']:
+#         offset = response['result'][0]['update_id']
+#         userText = response['result'][0]['message']['text']
+#         chatID = response['result'][0]['message']['chat']['id']
+#
+#         endPoint = f'https://api.telegram.org/bot{token}/sendAnimation'
+#         params = {'chat_id': chatID, "animation": giveAnimation(userText)}
+#         res = requests.get(endPoint, params=params)
+#         pprint.pprint(response['result'])
+#
+#     time.sleep(1)
