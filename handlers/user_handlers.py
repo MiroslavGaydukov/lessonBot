9be95941cb02e23441df540f6b5dcf58d40b9a86
mirikgitakk@gmmail.com <mirikgitakk@gmail.com>
2024-03-09 15:05:43 +0300

    Добавлен клавиатура

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,33 +2,27 @@ from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 from aiogram import Router, F
 from lexicon.lexicon import LEXICON_RU
+from keyboards.keyboards import yes_no_kb
 
 router = Router()
 # Этот хэндлер будет срабатывать на команду "/start"
 @router.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
-    await message.answer('Привет!Меня зовут Эхо-бот!\nНапиши мне что-нибудь')
-    await message.answer(LEXICON_RU['/start'])
+    await message.answer(LEXICON_RU['/start'], reply_markup=yes_no_kb)
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
 @router.message(Command(commands=['help']))
 async def process_help_command(message: Message):
-    await message.answer(
-        'Напиши мне что-нибудь и в ответ '
-        'я пришлю тебе твое сообщение'
-    )
     await message.answer(LEXICON_RU['/help'])
 
 
 def pelmenFilter(message: Message) -> bool:
     return message.text == 'пельмень'
 
-
 @router.message(pelmenFilter)
 async def send_echo(message: Message):
     await message.answer(text="Хочешь пельмешки?")
-
 
 @router.message(F.text == 'Да!')
 async def send_echo(message: Message):
